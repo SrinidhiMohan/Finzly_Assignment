1c780e905e2e60f3b11f73bab5fe915ac547a56a
Srinidhi Mohan <64879993+SrinidhiMohan@users.noreply.github.com>
2023-09-11 16:41:09 +0530

    Delete FXTrading/FXTrading directory

--- FXTrading/FXTrading/src/main/java/com/springboot/FXTrading/exception/TradeAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.FXTrading.exception;
-
-public class TradeAlreadyExistsException extends RuntimeException {
-
-	public TradeAlreadyExistsException(String msg) {
-		super(msg);
-	}
-	
-}
--- FXTrading/FXTrading/src/main/java/com/springboot/FXTrading/exception/TradeNotExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.FXTrading.exception;
-
-public class TradeNotExistsException extends RuntimeException {
-
-	public TradeNotExistsException(String msg) {
-		super(msg);
-	}
-
-}
